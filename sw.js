a917630f44adbf1d6f662a89e547f86e85e23fdb
oneill944 <64998555+oneill944@users.noreply.github.com>
2026-03-30 21:18:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-self.addEventListener('push', function(event) {
+// sw.js tartalma
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Ez fogadja a háttérüzeneteket
+self.addEventListener('push', (event) => {
     const data = event.data ? event.data.json() : {};
-    const title = data.title || "Új termék a kosárba!";
-    const options = {
-        body: data.body || "Nézd meg a listát!",
-        icon: 'IMG_4302.png',
-        badge: 'IMG_4302.png'
-    };
-    event.waitUntil(self.registration.showNotification(title, options));
+    event.waitUntil(
+        self.registration.showNotification(data.title || "Új termék a kosárba!", {
+            body: data.body || "Frissült a lista.",
+            icon: 'IMG_4302.png',
+            badge: 'IMG_4302.png'
+        })
+    );
 });
